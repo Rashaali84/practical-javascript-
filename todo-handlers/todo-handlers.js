38986663853311938b0927a9b2243705d013b2fc
Rasha ali <rashaali84@gmail.com>
2020-03-24 20:43:42 +0100

    add handlers

---
+++ todo-handlers/todo-handlers.js
@@ -0,0 +1,37 @@
+debugger;
+var handlers = {
+
+    addTodo: function () {
+        debugger;
+        var addTodoTextInput = document.getElementById('addTodoTextInput');
+        todoList.addTodo(addTodoTextInput.value);
+        addTodoTextInput.value = '';
+        view.displayTodos();
+    },
+    changeTodo: function () {
+        debugger;
+        var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+        var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+        todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+        changeTodoPositionInput.value = '';
+        changeTodoTextInput.value = '';
+        view.displayTodos();
+    },
+    deleteTodo: function (position) {
+        debugger;
+        todoList.deleteTodo(position);
+        view.displayTodos();
+    },
+    toggleCompleted: function () {
+        debugger;
+        var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        toggleCompletedPositionInput.value = '';
+        view.displayTodos();
+    },
+    toggleAll: function () {
+        debugger;
+        todoList.toggleAll();
+        view.displayTodos();
+    }
+};
